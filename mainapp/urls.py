17f3707f2e51430aee9798fbd74ef7222924622b
Andri Jasinski <andri.jasinski@gmail.com>
2018-04-24 20:15:47 +0300

    urls and views for all paths

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,11 @@ from mainapp import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'alarms/', views.alarms, name='alarms'),
+    url(r'positions/', views.positions, name='positions'),
+    url(r'params/', views.params, name='params'),
+    url(r'cctv/', views.cctv, name='cctv'),
+    url(r'curr_params/', views.curr_params, name='curr_params'),
+    url(r'changeable_settings/', views.changeable_settings, name='changeable_settings'),
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "mainapp/index.html")
+
+
+def alarms(request):
+    return render(request, "mainapp/alarms.html")
+
+
+def positions(request):
+    return render(request, "mainapp/positions.html")
+
+
+def params(request):
+    return render(request, "mainapp/params.html")
+
+
+def cctv(request):
+    return render(request, "mainapp/cctv.html")
+
+
+def changeable_settings(request):
+    return render(request, "mainapp/changeable_settings.html")
+
+
+def curr_params(request):
+    return render(request, "mainapp/curr_params.html")
