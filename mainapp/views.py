c581fae328eb872d33127651d19960bd7864d0ad
Andri Jasinski <andri.jasinski@gmail.com>
2018-05-09 21:35:41 +0300

    new colors for mobile view

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,14 +5,14 @@ def index(request):
     icon_size = 9
     row1 = [
         ['/alarms', 'mainapp/img/sample.png', 'fas fa-exclamation-triangle fa-%sx fa-fw' % icon_size, 'tomato'],
-        ['/positions', 'mainapp/img/sample.png', 'fas fa-map-marker-alt fa-%sx fa-fw' % icon_size, 'GreenYellow '],
-        ['/params', 'mainapp/img/sample.png', 'fas fa-chart-line fa-%sx fa-fw' % icon_size, 'blue'],
+        ['/positions', 'mainapp/img/sample.png', 'fas fa-map-marker-alt fa-%sx fa-fw' % icon_size, 'SpringGreen'],
+        ['/params', 'mainapp/img/sample.png', 'fas fa-chart-line fa-%sx fa-fw' % icon_size, 'BlueViolet'],
     ]
 
     row2 =[
-        ['/cctv', 'mainapp/img/sample.png', 'fas fa-video fa-%sx fa-fw' % icon_size, 'MediumSeaGreen'],
-        ['/changeable_settings', 'mainapp/img/sample.png', 'fas fa-toggle-on fa-%sx fa-fw' % icon_size, 'green'],
-        ['/curr_settings', 'mainapp/img/sample.png', 'fas fa-clipboard-list fa-%sx fa-fw' % icon_size, 'blue']
+        ['/cctv', 'mainapp/img/sample.png', 'fas fa-video fa-%sx fa-fw' % icon_size, 'DarkTurquoise '],
+        ['/changeable_settings', 'mainapp/img/sample.png', 'fas fa-toggle-on fa-%sx fa-fw' % icon_size, 'SlateGray'],
+        ['/curr_settings', 'mainapp/img/sample.png', 'fas fa-clipboard-list fa-%sx fa-fw' % icon_size, '#ed8f6d']
     ]
     return render(request, "mainapp/index.html", {"row1": row1, "row2": row2, "whole": row1 + row2})
 
